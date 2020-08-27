7478f6ceae09b4dde7a0dd3fe164fefadf557e99
VarinderKaur29 <Vk6964600@gmail.com>
2020-08-26 22:07:06 -0400

    Created Rating View Controller

---
+++ Capstone_GVA_PeerView/View_Controllers/RatingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RatingViewController.swift
+//  Capstone_GVA_PeerView
+//
+//  Created by Varinder Chahal on 2020-08-26.
+//  Copyright © 2020 Varinder Chahal. All rights reserved.
+//
+
+import UIKit
+
+class RatingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
